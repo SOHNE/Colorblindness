ff2e99cd171fb6f0df68ff97280a583ac9c11071
Leandro Peres <24868191+zschzen@users.noreply.github.com>
2020-06-18 13:49:43 -0300

    initial commit
    also v0.1

---
+++ Assets/__Scripts/Accessibility/Colorblind.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+namespace SOHNE.Accessibility.Colorblind
+{
+    public enum ColorblindTypes
+    {
+        Normal = 0,
+        Protanopia,
+        Protanomaly,
+        Deuteranopia,
+        Deuteranomaly,
+        Tritanopia,
+        Tritanomaly,
+        Achromatopsia,
+        Achromatomaly,
+    }
+
+    public class Colorblind : MonoBehaviour
+    {
+        Volume[] volumes;
+        VolumeComponent lastFilter;
+
+        int maxType;
+        int _currentType = 0;
+        int currentType
+        {
+            get => _currentType;
+
+            set
+            {
+                if (_currentType >= maxType) _currentType = 0;
+                else _currentType = value;
+            }
+        }
+
+        void SearchVolumes() => volumes = GameObject.FindObjectsOfType<Volume>();
+
+        #region Enable/Disable
+        private void OnEnable() => SceneManager.sceneLoaded += OnSceneLoaded;
+        #endregion
+
+        public static Colorblind Instance { get; private set; }
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            maxType = (int)System.Enum.GetValues(typeof(ColorblindTypes)).Cast<ColorblindTypes>().Last();
+        }
+
+        void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            SearchVolumes();
+
+            if (volumes == null || volumes.Length <= 0) return;
+
+            Change(-1);
+        }
+
+        void Start()
+        {
+            if (PlayerPrefs.HasKey("Accessibility.ColorblindType"))
+                currentType = PlayerPrefs.GetInt("Accessibility.ColorblindType");
+            else
+                PlayerPrefs.SetInt("Accessibility.ColorblindType", 0);
+
+            SearchVolumes();
+            StartCoroutine(ApplyFilter());
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.F1)) InitChange();
+        }
+
+        public void Change(int filterIndex = -1)
+        {
+            filterIndex = filterIndex <= -1 ? PlayerPrefs.GetInt("Accessibility.ColorblindType") : filterIndex;
+            currentType = Mathf.Clamp(filterIndex, 0, maxType);
+            StartCoroutine(ApplyFilter());
+        }
+
+        void InitChange()
+        {
+            if (volumes == null) return;
+            Debug.Log($"Modo daltônico [<b>{currentType}</b>/{maxType}]\n<b>{(ColorblindTypes)currentType}</b>");
+
+
+            StartCoroutine(ApplyFilter());
+
+            PlayerPrefs.SetInt("Accessibility.ColorblindType", currentType);
+            currentType++;
+        }
+
+        IEnumerator ApplyFilter()
+        {
+            ResourceRequest loadRequest = Resources.LoadAsync<VolumeProfile>($"Colorblind/{(ColorblindTypes)currentType}");
+
+            do yield return null; while (!loadRequest.isDone);
+            
+            var filter = loadRequest.asset as VolumeProfile;
+
+            if (filter == null)
+            {
+                Debug.LogError("An error has occured! Please, report");
+                yield break;
+            }
+
+            if (lastFilter != null)
+            {
+                foreach (var volume in volumes)
+                {
+                    volume.profile.components.Remove(lastFilter);
+
+                    foreach (var component in filter.components)
+                        volume.profile.components.Add(component);
+                }
+            }
+
+            lastFilter = filter.components[0];
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# Unity Colorblind
+[![Unity Version](https://img.shields.io/badge/Unity-2019.4%20LTS-green.svg?logo=unity&style=for-the-badge&colorA=000000)](https://store.unity.com/download?ref=personal)
+======
+
+Using the Channel Mixer component, includes eight color blindness profiles, based on this [Color Blindness Simulation][color-matrix].
+
+Currently, it only works with the following customizable render pipeline:
+  - Universal Render Pipeline (URP)
+  - High Definition Render Pipeline (HDRP) 
+
+# Features
+
+  - Load color filter profile in runtime pressing **F1** on keyboard.
+
+### Todos
+
+- [ ] Optimizations
+- [ ] Polishing
+- [ ] Documentation
+
+License
+----
+
+MIT
+
+
+**Live long and prosper \V/,**
+
+[//]: # (References)
+
+[color-matrix]: <https://web.archive.org/web/20081014161121/http:/www.colorjack.com/labs/colormatrix/>
